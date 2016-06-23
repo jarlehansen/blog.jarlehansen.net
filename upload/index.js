17f6eb72ce0d33445886f870b8120fb64b12e49e
jarlehansen <jarle@jarlehansen.net>
2016-06-23 23:48:03 +0200

    added script to deploy site

--- upload/index.js
+++ upload/index.js
@@ -1,15 +1,21 @@
-var FtpDeploy = require('ftp-deploy');
-var ftpDeploy = new FtpDeploy();
+const FtpDeploy = require('ftp-deploy');
+const ftpDeploy = new FtpDeploy();
 
-var config = {
-  username: "",
-  host: "",
-  port: 21,
-  localRoot: __dirname + "/../dist",
-  remoteRoot: "/blog/"
+console.log('Usage: npm start [host] [username] [password]');
+
+const config = {
+  host: process.argv[2],
+  username: process.argv[3],
+	password: process.argv[4],
+	port: 21,
+	localRoot: __dirname + "/../dist",
+	remoteRoot: "/blog"
 }
 
 ftpDeploy.deploy(config, function(err) {
-  if (err) console.log(err)
-  else console.log('finished');
-});
+	if (err) {
+    console.log(err);
+  } else {
+    console.log('Completed');
+  }
+}
