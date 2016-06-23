ddb8375e3d52d8a948a8b0e5ab97227444695e92
jarlehansen <jarle@jarlehansen.net>
2016-06-23 23:56:21 +0200

    updated upload script

--- upload/index.js
+++ upload/index.js
@@ -6,14 +6,14 @@ console.log('Usage: npm start [host] [username] [password]');
 const config = {
   host: process.argv[2],
   username: process.argv[3],
-	password: process.argv[4],
-	port: 21,
-	localRoot: `${__dirname}/../dist`,
-	remoteRoot: '/blog'
+  password: process.argv[4],
+  port: 21,
+  localRoot: `${__dirname}/../dist`,
+  remoteRoot: '/blog'
 }
 
 ftpDeploy.deploy(config, function(err) {
-	if (err) {
+  if (err) {
     console.log(err);
   } else {
     console.log('Completed');
